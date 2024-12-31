99decc2ca0f928babd4e7576845b666df28bd1e3
刘宁 <7610924+liuning1234561@user.noreply.gitee.com>
2024-12-31 18:08:45 +0800

    firstcommit

--- src/pages/user/list.vue
+++ src/pages/user/list.vue
@@ -1,32 +1,9 @@
 <template>
-  <div>
-    <div style="width: 500px; height: 500px">
-      <van-circle
-        v-model:current-rate="currentRate"
-        :rate="rate"
-        :speed="speed"
-        :text="text"
-      />
-    </div>
-    <van-button type="primary" @click="rateAdd()">加+</van-button>
-    <van-button type="primary" @click="rateCone()">减-</van-button>
-  </div>
+  <div class="comtainer"></div>
 </template>
+
 <script setup lang="ts">
-import { ref, computed } from "vue";
-const currentRate = ref(0);
-const rate = ref(20);
-const speed = ref(80);
-const text = computed(() => currentRate.value.toFixed(0) + "%");
-const rateAdd = () => {
-  rate.value += 10;
-  // currentRate.value = rate.value;
-  // speed.value = 80;
-};
-const rateCone = () => {
-  rate.value -= 10;
-  // currentRate.value = rate.value;
-  // speed.value = 80;
-};
+// import { ref, reactive } from 'vue'
 </script>
-<style scoped lang="scss"></style>
+
+<style lang="scss" scoped></style>
